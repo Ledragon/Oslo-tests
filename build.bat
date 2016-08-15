0a920b53cc84bee8600213e259b2718042b0ca99
Hugues Stefanski <hugues.stefanski@gmail.com>
2016-08-15 18:55:07 +0200

    WIP

--- build.bat
+++ build.bat
@@ -1,1 +1,3 @@
 "C:\Program Files\GDAL\ogr2ogr.exe" -f geojson oslo-buildings.json .\data\shapes\Oslo-shp\shape\buildings.shp
+
+"C:\Program Files\GDAL\ogr2ogr.exe" -f geojson data\norway-south-provinces.json data\shapes\ne_10m_admin_1_states_provinces\ne_10m_admin_1_states_provinces.shp -where "adm0_a3='NOR' AND latitude>0 and latitude<60"
--- src/app.js
+++ src/app.js
@@ -1,7 +1,45 @@
 (function () {
     'use strict';
+    var projection = d3.geoMercator()
+    // .scale(500)
+    // .translate([800, 600]);
+    var pathGenerator = d3.geoPath()
+        .projection(projection);
 
-    d3.select('body')
-        .append('div')
-        .text('from code');    
+    var svg = d3.select('svg');
+    d3.json('data/oslo.topojson', (error, data) => {
+        if (error) {
+            console.error(error);
+        } else {
+            var features = topojson.feature(data, data.objects['norway-south-provinces']);
+            console.log(features)
+            projection.fitSize([1600, 800], features);
+            console.log(projection.scale())
+            console.log(projection.translate())
+            svg.selectAll('path')
+                .data(features.features)
+                .enter()
+                .append('path')
+                // .attr('class', d => d.properties.type)
+                .attr('d', d => pathGenerator(d))
+                .on('mouseenter', function (d) {
+                    // console.log(d.properties);
+                    d3.select(this)
+                        .style('fill', 'red');
+                })
+                .on('mouseleave', function (d) {
+                    d3.select(this)
+                        .style('fill', '');
+                });
+            d3.json('data/oslo-places.json', (error, points) => {
+                svg.selectAll('.point')
+                    .data(points.features)
+                    .enter()
+                    .append('path')
+                    .attr('d', d => pathGenerator(d));
+            })
+        }
+
+    })
+
 } ());
